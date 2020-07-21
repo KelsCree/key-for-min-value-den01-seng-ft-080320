091190e00a1d2719b386ea14a3c1fb37571ce835
Kelsey Creehan <kelseycreehan@gmail.com>
2020-07-21 00:37:57 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,10 +3,8 @@
 
 def key_for_min_value(name_hash)
 lowest_number = nil
-answer = 
 name_hash.each do |name, number|
  if lowest_number == nil
-  answer = name
   lowest_number = number
  elsif lowest_number > number
    answer = name
